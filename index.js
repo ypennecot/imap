d18289b9edb0af442e64cba32660a0b2499c4c90
yannick <yannick@hedonic-technologies.com>
2015-03-15 21:44:06 +0100

    first push

---
+++ index.js
@@ -0,0 +1,22 @@
+var SimpleImap = require('simple-imap');
+
+var options = {
+    user: 'eugeniebastille@gmail.com',
+    password: 'hedotech78',
+    host: 'smtp.gmail.com',
+    port: 993,
+    tls: true,
+    mailbox: 'INBOX'
+};
+
+var simpleImap = new SimpleImap(options);
+
+simpleImap.on('error', function(err) {
+    console.log(err);
+});
+
+simpleImap.on('mail', function(mail) {
+    console.log(mail);
+});
+
+simpleImap.start();
